8637a19e494fb52e44f4139e127f7ab7e450fabf
Jana <janazeineddin55@gmail.com>
2025-07-03 14:27:59 +0300

    exercise 3 easy

--- exercise3-easy.py
+++ exercise3-easy.py
@@ -1,8 +1,1 @@
-lt_num = [34,10,53,25,17,5]
-new_lt =[]
-def firstlast_num (lt_num, new_lt):
-      new_lt.append(lt_num[0])
-      new_lt.append(lt_num[-1])
-      print(new_lt)
 
-firstlast_num(lt_num,new_lt)
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-name =""
-age =""
+lt_num = [34,10,53,25,17,5]
+new_lt =[]
+def firstlast_num (lt_num, new_lt):
+      new_lt.append(lt_num[0])
+      new_lt.append(lt_num[-1])
+      print(new_lt)
 
-name = input("What is your name?")
-age = input("What is your age?")
-
-print("The user " + name + " is "+ age + " years old.")
+firstlast_num(lt_num,new_lt)
